dac41511d8d47598e21faf2f0bb4c67921549377
Mohamed Dhia <dhiaweslati16@gmail.com>
2021-08-17 20:03:20 +0100

    no build

--- build/_snowpack/pkg/react.js
+++
@@ -1,8 +0,0 @@
-import { r as react } from './common/index-8af8b000.js';
-export { r as default } from './common/index-8af8b000.js';
-
-
-
-var useRef = react.useRef;
-var useState = react.useState;
-export { useRef, useState };
